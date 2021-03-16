d5329f9cb84f04a03fcc3576feee81a505c6fed6
Jonas Bushart <jonas@bushart.org>
2021-03-16 23:22:40 +0100

    Add tiny templating system
    Add two comments like
    ```html
    <!-- TEMPLATE HEAD START -->
    <!-- TEMPLATE HEAD END -->
    ```
    to the html file where the template should be inserted.
    The template name in this case is `head`.
    Create a `head.html` in the templates folder.
    
    Everything between those two comments will be replaced by the content of the template file.

---
+++ scripts/build.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+import typing as t
+from pathlib import Path
+from glob import glob
+
+
+def main() -> None:
+    files = find_html_files()
+    update_templates(files)
+
+
+def find_html_files() -> t.List[Path]:
+    """
+    Return all files ending in .html except the ones in the templates folder
+    """
+    files = []
+    for filepath in glob("../**/*.html", recursive=True):
+        path = Path(filepath)
+        if path.parent.name == "templates":
+            continue
+        files.append(path)
+
+    return files
+
+
+def update_templates(files: t.List[Path]) -> None:
+    templates = {}
+
+    # Find all available templates
+    for filepath in glob("../templates/*.html"):
+        path = Path(filepath)
+        # Get filename without suffix
+        template_name = path.with_suffix("").name
+        with path.open("rt") as f:
+            template_content = f.read()
+        templates[template_name] = template_content
+
+    for file in files:
+        with file.open("r+") as f:
+            content = f.read()
+            for tpl_name, tpl in templates.items():
+                start = f"<!-- TEMPLATE {tpl_name.upper()} START -->"
+                end = f"<!-- TEMPLATE {tpl_name.upper()} END -->"
+
+                offset = 0
+                while (offset := content.find(start, offset)) != -1:
+                    offset += len(start)
+                    if (end_offset := content.find(end, offset)) != -1:
+                        content = content[:offset] + "\n" + tpl + content[end_offset:]
+
+            f.seek(0)
+            f.write(content)
+            # Strip remaining content from the file
+            f.truncate()
+
+
+if __name__ == "__main__":
+    main()
