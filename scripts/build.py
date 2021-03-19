083647d3cc66e0ec3acfbe08def514292da5b200
Jonas Bushart <jonas@bushart.org>
2021-03-19 21:23:04 +0100

    Create a build process which only copies necessary files.
    Create an output folder `build`.
    Copy all HTML files.
    Copy "important" folders, currently css and images.
    
    Fixes #2

--- scripts/build.py
+++ scripts/build.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python3
 from typing import List
+import shutil
 from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).parent.parent.absolute()
+OUTPUT = PROJECT_ROOT.joinpath("build")
 
 
 def main() -> None:
+    # Delete the output folder.
+    # Otherwise it might interfere when searching for files later
+    shutil.rmtree(OUTPUT, ignore_errors=True)
+
     files = find_html_files()
     update_templates(files)
+    create_build_output(files)
 
 
 def find_html_files() -> List[Path]:
     """
-    Return all files ending in .html except the ones in the templates folder
+    Return all files ending in .html except the ones in the templates folder.
     """
     files = []
-    for path in PROJECT_ROOT.glob("*.html"):
-        print(path)
+    for path in PROJECT_ROOT.glob("**/*.html"):
         if path.parent.name == "templates":
             continue
         files.append(path)
@@ -57,6 +63,18 @@ def update_templates(files: List[Path]) -> None:
             f.write(content)
             # Strip remaining content from the file
             f.truncate()
+
+
+def create_build_output(html_files: List[Path]) -> None:
+    OUTPUT.mkdir()
+    for folder in ["css", "images"]:
+        shutil.copytree(PROJECT_ROOT.joinpath(folder), OUTPUT.joinpath(folder))
+    # Copy HTML files
+    for html_file in html_files:
+        output_html_file = OUTPUT.joinpath(html_file.relative_to(PROJECT_ROOT))
+        # Ensure the folder exists
+        output_html_file.parent.mkdir(parents=True, exist_ok=True)
+        shutil.copy(html_file, output_html_file)
 
 
 if __name__ == "__main__":
